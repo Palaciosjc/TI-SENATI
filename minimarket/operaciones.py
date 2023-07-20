283519d29a300cb8409f8c8f22df10d71d8a20be
Placiosjc <1547535@senati.pe>
2023-07-20 12:58:15 -0500

    ventas

--- minimarket/operaciones.py
+++ minimarket/operaciones.py
@@ -1,37 +1,50 @@
-from productos import crearproductos, listaproductos
-
+from productos import crearproductos, listarproductos
 
 def menu():
-    lista_de_productos=[]
-
+    lista_de_productos = []
     omenu = """
     MINIMARKET
     ___________________
-    1.listar producto
-    2.Agregar producto
-    3.Salir
+    1.Listar productos
+    2.Agregar productos
+    3.Venta
+    4.Salir
     """
     flag = True
     while flag:
         try:
-             print(omenu)
-             opcion = int(input("elige una opcion:"))
+            print(omenu)
+            opcion = int(input("Elige una opcion: "))
         except ValueError:
-            print('has elegido una opcion que no esta en el menu')
+            print('Has elegido una opcion que no esta en el menu')
         else:
             if opcion == 1:
-                print("listado productos")
-                listaproductos(lista_de_productos)
-
-            elif opcion ==2:
-                print("agragando productos")
+                print("Listando productos")
+                print(lista_de_productos)
+                listarproductos(lista_de_productos)
+            elif opcion == 2:
+                print("Agregando productos")
                 crearproductos(lista_de_productos)
-            elif opcion ==3:
-                print("saliendo")  
+            elif opcion == 3:
+                print("Iniciar Venta")
+                lista_venta = []
+                producto_venta = {}
+                listarproductos(lista_de_productos)
+                seleccionproducto = int(input('elija el numero del producto: '))
+                producto = (lista_de_productos[seleccionproducto-1])
+                cantidadproducto = int(input(f'escriba cantidad de {producto.get("nombre")}: '))
+                producto["cantidad"] = producto.get("cantidad") - cantidadproducto
+                producto_venta["producto"] = producto.ge("nombre")
+                producto_venta["cantidad"] = cantidadproducto
+                producto_venta["subtotal"] = cantidadproducto * producto.get("costo")
+                lista_venta.append(producto_venta)
+                listarproductos(lista_de_productos)
+                print(lista_venta)
+
+
+
+            elif opcion == 4:
+                print("Saliendo")
                 flag = False
-
-
-menu()             
-
-
+menu()
 
--- minimarket/productos.py
+++ minimarket/productos.py
@@ -1,59 +1,58 @@
 def crearproductos(lista_productos):
     producto = {}
-
     flag=True
     while flag:
-        try:
-                nombreproducto = input("nombre del producto")
-                marcaproducto = input("marca del producto")
-                costoproducto = numerosdenentrada("costo del producto")
-                cantidadproducto = numerosdenentrada("cantidad del producto")
-
-        
-
+        try: 
+            nombreproducto = input("Nombre del producto: ")
+            marcaproducto = input("Marca del producto: ")
+            costoproducto = numerosdenetrada("Costo del producto: ")
+            cantidadproducto = numerosdenetrada("Cantidad del producto: ")
         except ValueError:
-         print("algo salio mal intentalo otra vez")
+            print("ALGO SALIO MAL INTENTALO OTRA VEZ")
         else:
             producto["nombre"]= nombreproducto
-            producto ["marca"]= marcaproducto
-            producto ["costo"]= costoproducto
-            producto ["cantidad"]= cantidadproducto
+            producto["marca"]= marcaproducto
+            producto["costo"]= costoproducto
+            producto["cantidad"]= cantidadproducto
             flag_repeticion = coincidenciaproducto(nombreproducto, cantidadproducto, lista_productos)
             if not flag_repeticion:
                 lista_productos.append(producto)
-        producto = {}
-        pregunta = input("desea agrear mas productos? SI/NO")
-        if str(pregunta) != "si":
-            flag = False
-            print(lista_productos)
+            producto = {}
+            pregunta = input("Desea agrear mas Productos? SI/NO")
+            if str(pregunta) != "SI":
+                flag = False
 
     return lista_productos
-def listaproductos(lista_productos):
-    print('producto | cantidad | precio')
-    contador = 0
+
+
+def listarproductos(lista_productos):
+    print('Producto | Cantidad | Precio')
     for producto in lista_productos:
-        contador +=1
         print(f"{producto.get('nombre')} | {producto.get('cantidad')} | {producto.get('costo')}")
 
-    
-    
-def numerosdenentrada(mensaje):
-
+def numerosdenetrada(mensaje): 
     while True:
         try:
-            numero_ingreso = input(input(mensaje))
+            numero_ingreso = int(input(mensaje))
         except ValueError:
             print("te equivocaste esto debe ser un numero")
         else:
-          break
+            break
     return numero_ingreso
 
-def coincidenciaproducto(nombreproducto, marcaproducto,cantidadproducto,lista_producto):
+numerosdenetrada("")
+
+def coincidenciaproducto(nombreproducto, cantidadproducto, lista_productos):
     flag = False
-    for producto in lista_producto:
-       if producto.get("nombre","marca")== nombreproducto and marcaproducto:
-          print("este producto ya esta registrado")
-          producto["cantidad"]=producto.get("cantidad") + cantidadproducto
-          flag = True
-       return flag     
+    for producto in lista_productos:
+        if producto.get("nombre") == nombreproducto:
+            print("este producto ya esta registrado")
+            producto["cantidad"] = producto.get('cantidad') + cantidadproducto
+            flag = True
+    return flag
+
+
+
+
+
 
